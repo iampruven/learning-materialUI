f9d7e48a1bee94b0aefdb1c6b7cd64de699e9683
Vendy Prum <vendy@thinkful.com>
2021-04-20 16:48:00 -0700

    navigation functionality with react router

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,24 @@
 import Header from "../components/ui/Header";
 import { ThemeProvider } from "@material-ui/core/styles";
-import theme from './ui/Theme'
+import theme from "./ui/Theme";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Header />
-      Hello
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={()=><div>HOMEE</div>} />
+          <Route exact path="/services" component={()=><div>Servicess</div>} />
+          <Route exact path="/customesoftware" component={()=><div>Custom Software</div>} />
+          <Route exact path="/mobileapps" component={()=><div>Mobile Apps</div>} />
+          <Route exact path="/websites" component={()=><div>Websites</div>} />
+          <Route exact path="/revolution" component={()=><div>Revolution</div>} />
+          <Route exact path="/about" component={()=><div>About</div>} />
+          <Route exact path="/contact" component={()=><div>Contact</div>} />
+          <Route exact path="/estimate" component={()=><div>Estimate</div>} />
+        </Switch>
+      </Router>
     </ThemeProvider>
   );
 }
--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,6 +1,6 @@
-import React from "react";
-import logo from '../../assets/logo.svg';
-
+import React, { useEffect, useState } from "react";
+import logo from "../../assets/logo.svg";
+import { Link } from "react-router-dom";
 import {
   AppBar,
   Button,
@@ -25,44 +25,119 @@ function ElevationScroll(props) {
 const useStyles = makeStyles((theme) => ({
   toolBarMargin: {
     ...theme.mixins.toolbar,
-    marginBottom:"3em"
+    marginBottom: "3em",
   },
   logo: {
-    height:"7em"
+    height: "8em",
   },
-  tabContainer:{
-    marginLeft:'auto'
+  logoContainer: {
+    padding: 0,
+    "&:hover":{
+      backgroundColor:"transparent"
+    }
   },
-  tab:{
+  tabContainer: {
+    marginLeft: "auto",
+  },
+  tab: {
     ...theme.typography.tab,
     minWidth: 10,
-    marginLeft:"25px" //to keep consistent use px , if want responsive use rem etc.
+    marginLeft: "25px", //to keep consistent use px , if want responsive use rem etc.
   },
-  button:{
-    borderRadius:"50px",
-    marginLeft:"50px",
-    marginRight:"25px",
+  button: {
+    borderRadius: "50px",
+    marginLeft: "50px",
+    marginRight: "25px",
     ...theme.typography.estimate,
-    height:"45px",
-  }
+    height: "45px",
+  },
 }));
 
 export default function Header(props) {
   const classes = useStyles();
+  const [value, setValue] = useState(0);
+
+  const handleChange = (e, value) => {
+    setValue(value);
+  };
+
+  useEffect(() => {
+    if (window.location.pathname === "/" && value !== 0) {
+      setValue(0);
+    } else if (window.location.pathname === "/services" && value !== 1) {
+      setValue(1);
+    } else if (window.location.pathname === "/revolution" && value !== 2) {
+      setValue(2);
+    } else if (window.location.pathname === "/about" && value !== 3) {
+      setValue(3);
+    } else if (window.location.pathname === "/contact" && value !== 4) {
+      setValue(4);
+    } else if (window.location.pathname === "/estimate" && value !== 5) {
+      setValue(5);
+    }
+  }, [value]); //only run when the value changes
   return (
     <>
       <ElevationScroll>
         <AppBar position="fixed">
           <Toolbar disableGutters>
-            <img src={logo} className={classes.logo} alt="company logo"/>
-            <Tabs className={classes.tabContainer}>
-              <Tab className={classes.tab} label="Home" />
-              <Tab className={classes.tab} label="Services" />
-              <Tab className={classes.tab} label="The Revolution" />
-              <Tab className={classes.tab} label="About Us" />
-              <Tab className={classes.tab} label="Contact Us" />
+            <Button
+              disableRipple
+              className={classes.logoContainer}
+              component={Link}
+              to="/"
+            >
+              <img
+                onClick={() => setValue(0)}
+                src={logo}
+                className={classes.logo}
+                alt="company logo"
+              />
+            </Button>
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              indicatorColor="primary"
+              className={classes.tabContainer}
+            >
+              <Tab
+                className={classes.tab}
+                label="Home"
+                component={Link}
+                to="/"
+              />
+              <Tab
+                className={classes.tab}
+                label="Services"
+                component={Link}
+                to="/services"
+              />
+              <Tab
+                className={classes.tab}
+                label="The Revolution"
+                component={Link}
+                to="/revolution"
+              />
+              <Tab
+                className={classes.tab}
+                label="About Us"
+                component={Link}
+                to="/about"
+              />
+              <Tab
+                className={classes.tab}
+                label="Contact Us"
+                component={Link}
+                to="/contact"
+              />
             </Tabs>
-            <Button variant="contained" color="secondary" className={classes.button}>Free Estimate</Button>
+            <Button
+              variant="contained"
+              color="secondary"
+              className={classes.button}
+            >
+              Free Estimate
+            </Button>
           </Toolbar>
         </AppBar>
       </ElevationScroll>
